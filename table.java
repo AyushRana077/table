9526bdf15af81f1867be1e942a9058508540e452
Ayush Rana <ayushrana0777@gmail.com>
2024-09-27 13:14:30 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class table {
+    private static int nextInt;
+
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n = sc.nextInt();
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i*n);
+           
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
